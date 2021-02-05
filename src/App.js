a18b6bb21d44f6ab6ac8f99332646c6e59c3fe0d
Victoria Rouser <vic86rous@gmail.com>
2021-02-04 21:31:28 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import EmployeeDirectory from "./pages/ProfileDirectory";
+import "./App.css"
+
+const App = () => {
+  return (
+  <EmployeeDirectory />
+  )
+};
+  
+export default App;
---
+++ src/utils/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export default {
+  // Retrieve 20 random users
+  getEmployees: function () {
+    
+    return axios.get('https://randomuser.me/api/?results=20&nat=us');
+  
+  }
+
+};
